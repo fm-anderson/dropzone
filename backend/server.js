f8e00287613277c8672186ca11d7bb49e07ee529
Anderson M <78233612+fm-anderson@users.noreply.github.com>
2024-03-29 16:41:48 -0400

    cors config added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
+const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+
+app.use(cors());
+
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("DropZone by fm-anderson");
